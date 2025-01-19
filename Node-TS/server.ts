cca5e571d07c15a0ac79fa02f0c555b909cb9952
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2025-01-19 09:50:40 +0700

    Update server.ts

--- Node-TS/server.ts
+++ Node-TS/server.ts
@@ -5,12 +5,12 @@ import { routes } from "./route";
 const initServer = async (): Promise<void> => {
   const server = hapi.server({
     port: 3000,
+    host: "localhost",
   });
 
   server.route(routes);
 
   await server.start();
-  console.log("server start");
+  console.log(`server start in ${server.info.uri}`);
 };
-
 initServer();
